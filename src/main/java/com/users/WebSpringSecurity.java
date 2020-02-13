7ecbc9a620fa69ca40172497350d3d4bb3fc135b
NicksComingUp <remember.marsman@gmail.com>
2020-02-13 00:40:46 -0500

    ignoring some routes so that spring security can do its thing

--- src/main/java/com/users/WebSpringSecurity.java
+++ src/main/java/com/users/WebSpringSecurity.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
@@ -24,13 +25,13 @@ import com.users.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSpringSecurity extends WebSecurityConfigurerAdapter {
 	private Environment  environment;
 	private UserService userService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
-	public WebSecurity(Environment environment,
+	public WebSpringSecurity(Environment environment,
 			UserService userService,
 			BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.environment = environment;
@@ -42,12 +43,21 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
 	}
+	
+	// lets the filters ignore the /auth/signup and check so that people are able to register
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/auth/signup").antMatchers("/auth");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 //		super.configure(http);
 		http.csrf().disable();
+		
 		http.headers().frameOptions().disable();
+		
 		 http.cors().configurationSource(corsConfigurationSource()).and().authorizeRequests()
 		.antMatchers("/auth/**").permitAll()
 		.and()
